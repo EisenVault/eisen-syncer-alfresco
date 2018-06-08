17f1245e3aa1a2338e704a03fd734fa7e9132f4e
Soubhik Chatterjee <soubhik.chatterjee@eisenvault.com>
2018-06-08 15:30:58 +0530

    path changse

--- server/config/db.js
+++ server/config/db.js
@@ -4,6 +4,7 @@ exports.db = require("knex")({
   client: "sqlite3",
   connection: {
     filename: path.join(__dirname.replace("config", "database"), "syncer.db")
+    // filename: path.join(__dirname.replace('app.asar', 'app.asar.unpacked').replace('config', 'database'), "syncer.db")
   },
   useNullAsDefault: true
 });
