2b0c3b0bc8ec25179b937c790c5be4932a3761cb
Soubhik Chatterjee <soubhik.chatterjee@eisenvault.com>
2018-07-12 15:32:54 +0530

    Implemented cors

--- service/service.js
+++ service/service.js
@@ -16,21 +16,3 @@ io.on("connection", client => {
     client.broadcast.emit("sync-notification", data);
   });
 });
-
-// ...
-// var express = require("express");
-// var app = express();
-// var server = require("http").Server(app);
-// var io = require("socket.io")(server, {
-//   origins: "localhost: http://localhost:* http://www.localhost:*"
-// });
-
-// io.on("connection", client => {
-//   client.on("sync-notification", data => {
-//     client.broadcast.emit("sync-notification", data);
-//   });
-// });
-
-// server.listen(3002, "", function() {
-//   console.log("Server up and running...");
-// });
